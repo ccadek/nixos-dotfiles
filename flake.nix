ce423428477ff687b4a74c775a39ead8830ffc10
cc <purzelification@gmail.com>
2024-06-04 14:29:17 +0200

    Adds Tower VM configuration

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.05";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -15,6 +15,19 @@
       system = "x86_64-linux";
       modules = [
         ./hosts/laptop/configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.backupFileExtension = "bk";
+          home-manager.users.chris = import ./home/chris.nix;
+        }
+      ];
+    };
+    nixosConfigurations.tower-vm = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./hosts/tower-vm/configuration.nix
         home-manager.nixosModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
